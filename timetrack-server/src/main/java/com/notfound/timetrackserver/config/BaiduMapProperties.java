df4a65aeffb598c7fb1f193ca020633cf9675bd4
陈滕烁 <23373257@buaa.edu.cn>
2026-05-02 16:46:59 +0800

    feat:删除代码中尚未完成的B-10功能的相关代码

--- timetrack-server/src/main/java/com/notfound/timetrackserver/config/BaiduMapProperties.java
+++ timetrack-server/src/main/java/com/notfound/timetrackserver/config/BaiduMapProperties.java
@@ -1,7 +1,5 @@
 package com.notfound.timetrackserver.config;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "baidu.map")
 public record BaiduMapProperties(String ak, String geocoderUrl, String placeSearchUrl) {
 }
 
