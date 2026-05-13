3394ea23d6d9e6ffae96b1f4c6abc4986b83852e
Kurna <kurna@foxmail.com>
2026-05-13 15:12:16 +0800

    [feature]: add icp info in page

--- timetrack-ui/src/App.vue
+++ timetrack-ui/src/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <router-view />
+  <div class="app-shell">
+    <div class="app-content">
+      <router-view />
+    </div>
+    <footer class="site-footer">
+      <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">
+        京ICP备2026018715号
+      </a>
+    </footer>
+  </div>
 </template>
